bfbeed11ce0f0308640dcfc4abea68cd21aea259
BigBoiJonny <jonny98.0@outlook.com>
2023-04-03 15:34:09 +0100

    kinda added front end stuff

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,63 @@
+import axios from "axios";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 const Login = () => {
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userData, setUserData] = useState("");
+  const router = useRouter();
+
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    if (!loginEmail || !loginPassword) {
+      setErrorMessage("You must provide a username and a password");
+      return;
+    }
+
+    try {
+      const { data } = await axios.post(`http://localhost:3001/api/user`, {
+        email: loginEmail,
+        password: loginPassword,
+      });
+      sessionStorage.setItem("authToken", data.authToken);
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.log(error.response);
+      setErrorMessage(error.response.data.message);
+    }
+  };
+
+  const getUserDetails = async () => {
+    const authToken = sessionStorage.getItem("authToken");
+    try {
+      const { data } = await axios.get(`http://localhost:3001/api/user`, {
+        headers: {
+          authorisation: `Bearer ${authToken}`,
+        },
+      });
+      setUserData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (isLoggedIn) {
+    getUserDetails().then(() =>
+      router.push({
+        pathname: "/profile",
+        query: { userId: userData.id },
+        state: userData,
+      })
+    );
+  }
+
   return (
     <div>
       <h1>Login</h1>
-      <form action="">
+      <form onSubmit={loginHandler}>
         <input
           onChange={(e) => setLoginEmail(e.target.value)}
           type="email"
@@ -19,6 +70,8 @@ const Login = () => {
           name="password"
           placeholder="password"
         />
+
+        <button type="submit">Login</button>
       </form>
     </div>
   );
--- src/pages/profile/[userId].js
+++ src/pages/profile/[userId].js
@@ -1,12 +1,29 @@
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Profile = () => {
-  const router = useRouter();
-  const { username } = router.query;
+  const [isLoading, setIsLoading] = useState(true);
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const authToken = sessionStorage.getItem("authToken");
+      try {
+        const { data } = await axios.get("http://localhost:3001/api/user", {
+          headers: { authorisation: `Bearer ${authToken}` },
+        });
+        setUserData(data);
+        setIsLoading(false);
+      } catch {}
+    };
+    getUserData();
+  }, []);
 
   return (
     <>
-      <h1>hello {username} !</h1>
+      {isLoading && <h1>LOADING...</h1>}
+      {!isLoading && <h1>hello {userData.name} !</h1>}
     </>
   );
 };
